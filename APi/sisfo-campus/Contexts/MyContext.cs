b5743c9530dad3a6afa6986079aa7e8f04a365f2
ronaldowijaya <ronaldoanol@gmail.com>
2023-04-10 14:10:01 +0700

    }

--- APi/sisfo-campus/Contexts/MyContext.cs
+++ APi/sisfo-campus/Contexts/MyContext.cs
@@ -14,46 +14,47 @@ public class MyContext : DbContext
     public DbSet<Major> Majors { get; set; }
     public DbSet<Assignment> Assignments { get; set; }
     public DbSet<Role> Roles { get; set; }
-/*    public DbSet<Student> Students { get; set; }
-    public DbSet<Course> Courses { get; set; }
-    public DbSet<Faculty> Facultys { get; set; }
-    public DbSet<Lecturer> Lecturers { get; set; }*/
-/*
-    // Fluent API
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<Role>().HasData(
-            new Role
-            {
-                Id = 1,
-                Name = "Admin",
-            },
-            new Role
-            {
-                Id = 2,
-                Name = "Lecturer",
-            },
-            new Role
-            {
-                Id = 3,
-                Name = "Student",
-            });
-
-        // Membuat atribute menjadi unique
-        modelBuilder.Entity<Student>().HasIndex(e => new
+    /*    public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Faculty> Facultys { get; set; }
+        public DbSet<Lecturer> Lecturers { get; set; }*/
+    /*
+        // Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            e.Email,
-            e.PhoneNumber
-        }).IsUnique();
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Role>().HasData(
+                new Role
+                {
+                    Id = 1,
+                    Name = "Admin",
+                },
+                new Role
+                {
+                    Id = 2,
+                    Name = "Lecturer",
+                },
+                new Role
+                {
+                    Id = 3,
+                    Name = "Student",
+                });
+
+            // Membuat atribute menjadi unique
+            modelBuilder.Entity<Student>().HasIndex(e => new
+            {
+                e.Email,
+                e.PhoneNumber
+            }).IsUnique();
 
 
-        // Relasi one Employee ke one Account 
-        modelBuilder.Entity<Student>()
-            .HasOne(e => e.Account)
-            .WithOne(a => a.Student)
-            .HasForeignKey<Account>(fk => fk.StudentNim);
-*/
-    }
+            // Relasi one Employee ke one Account 
+            modelBuilder.Entity<Student>()
+                .HasOne(e => e.Account)
+                .WithOne(a => a.Student)
+                .HasForeignKey<Account>(fk => fk.StudentNim);
+
+        }
+    */
 }
